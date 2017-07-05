5f8a12f177046f4ee6dd051dfaf381df580c1a83
Justin Rhoads <rhoadsjustin@gmail.com>
2017-07-05 10:54:03 -0500

    fixed erros of routing

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 import { Provider, connect } from 'react-redux';
 import { Font, AppLoading } from 'expo';
-import store from './app/redux/store';
-import Router from './app/config/routes';
+import store from './redux/store';
+import Router from './config/routes';
 
 export default class App extends Component {
   constructor(){
@@ -17,7 +17,7 @@ export default class App extends Component {
     await Font.loadAsync({
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      'Pacifico': require('./app/assets/fonts/Pacifico.ttf'),
+      'Pacifico': require('./assets/fonts/Pacifico.ttf'),
       'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
     });
 
